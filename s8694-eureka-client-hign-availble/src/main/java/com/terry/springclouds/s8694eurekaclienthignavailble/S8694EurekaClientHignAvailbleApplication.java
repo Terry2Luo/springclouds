198ad276d9cb8114845141d25715555e4279f870
luxing <Luxing123456>
2020-05-21 16:22:55 +0800

    注册中心高可用-客户端

---
+++ s8694-eureka-client-hign-availble/src/main/java/com/terry/springclouds/s8694eurekaclienthignavailble/S8694EurekaClientHignAvailbleApplication.java
@@ -0,0 +1,28 @@
+package com.terry.springclouds.s8694eurekaclienthignavailble;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class S8694EurekaClientHignAvailbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S8694EurekaClientHignAvailbleApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+}
---
+++ s8694-eureka-client-hign-availble/src/test/java/com/terry/springclouds/s8694eurekaclienthignavailble/S8694EurekaClientHignAvailbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terry.springclouds.s8694eurekaclienthignavailble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S8694EurekaClientHignAvailbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
