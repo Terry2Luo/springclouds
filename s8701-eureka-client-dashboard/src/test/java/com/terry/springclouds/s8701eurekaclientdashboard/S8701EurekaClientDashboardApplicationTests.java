ac00b391bbab3e549260b12a12e0e48c7da21530
luxing <Luxing123456>
2020-05-26 15:23:11 +0800

    断路器监控(Hystrix Dashboard)

---
+++ s8701-eureka-client-dashboard/src/test/java/com/terry/springclouds/s8701eurekaclientdashboard/S8701EurekaClientDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terry.springclouds.s8701eurekaclientdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S8701EurekaClientDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
